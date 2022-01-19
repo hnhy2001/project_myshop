b31a5681dacad3f7653f28bf553dcd7776cfa683
manh <manhdop01@gmail.com>
2022-01-19 21:52:44 +0700

    manh-commit

---
+++ dumps/Dump20220119/myshop_django_migrations.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: myshop
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `django_migrations`
+--
+
+DROP TABLE IF EXISTS `django_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `django_migrations` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `django_migrations`
+--
+
+LOCK TABLES `django_migrations` WRITE;
+/*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2022-01-15 16:24:07.379279'),(2,'contenttypes','0002_remove_content_type_name','2022-01-15 16:24:07.484664'),(3,'auth','0001_initial','2022-01-15 16:24:07.899476'),(4,'auth','0002_alter_permission_name_max_length','2022-01-15 16:24:08.009190'),(5,'auth','0003_alter_user_email_max_length','2022-01-15 16:24:08.023144'),(6,'auth','0004_alter_user_username_opts','2022-01-15 16:24:08.032119'),(7,'auth','0005_alter_user_last_login_null','2022-01-15 16:24:08.047086'),(8,'auth','0006_require_contenttypes_0002','2022-01-15 16:24:08.056057'),(9,'auth','0007_alter_validators_add_error_messages','2022-01-15 16:24:08.064034'),(10,'auth','0008_alter_user_username_max_length','2022-01-15 16:24:08.076003'),(11,'auth','0009_alter_user_last_name_max_length','2022-01-15 16:24:08.083680'),(12,'auth','0010_alter_group_name_max_length','2022-01-15 16:24:08.115595'),(13,'auth','0011_update_proxy_permissions','2022-01-15 16:24:08.130560'),(14,'auth','0012_alter_user_first_name_max_length','2022-01-15 16:24:08.141526'),(15,'shopapp','0001_initial','2022-01-15 16:24:09.280235'),(16,'admin','0001_initial','2022-01-15 16:24:09.461045'),(17,'admin','0002_logentry_remove_auto_add','2022-01-15 16:24:09.491062'),(18,'admin','0003_logentry_add_action_flag_choices','2022-01-15 16:24:09.508015'),(19,'sessions','0001_initial','2022-01-15 16:24:09.689418'),(20,'shopapp','0002_notication_casee_notication_updated_date_and_more','2022-01-15 16:24:09.849317'),(21,'shopapp','0003_alter_user_cart','2022-01-15 16:24:09.862317'),(22,'shopapp','0004_order_active','2022-01-15 16:24:09.915450'),(23,'shopapp','0005_product_content','2022-01-15 16:24:09.970700'),(24,'shopapp','0006_order_phone','2022-01-15 16:24:10.044752'),(25,'shopapp','0007_alter_user_cart','2022-01-15 16:31:46.002651'),(26,'shopapp','0008_alter_order_product_id','2022-01-15 16:31:46.020610'),(27,'shopapp','0009_alter_product_product_price','2022-01-19 14:12:47.713632');
+/*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-19 21:39:47
---
+++ dumps/Dump20220119/myshop_shopapp_order.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: myshop
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shopapp_order`
+--
+
+DROP TABLE IF EXISTS `shopapp_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shopapp_order` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `order_price` double NOT NULL,
+  `user_id` bigint NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `shopapp_order_user_id_8d665b62_fk_shopapp_user_id` (`user_id`),
+  CONSTRAINT `shopapp_order_user_id_8d665b62_fk_shopapp_user_id` FOREIGN KEY (`user_id`) REFERENCES `shopapp_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shopapp_order`
+--
+
+LOCK TABLES `shopapp_order` WRITE;
+/*!40000 ALTER TABLE `shopapp_order` DISABLE KEYS */;
+INSERT INTO `shopapp_order` VALUES (1,123134124,4,0,NULL),(2,123134124,3,0,NULL),(3,123134124,3,0,NULL),(4,123134124,3,0,NULL),(5,59000,3,0,NULL),(6,100000,3,0,NULL),(7,68000,3,0,NULL),(8,39000,3,0,NULL),(9,54000,3,0,NULL),(10,183000,3,0,NULL),(11,62000,3,0,NULL),(12,62000,3,0,NULL),(13,54000,3,0,NULL),(14,54000,3,0,NULL),(15,120000,10,0,NULL),(16,54000,10,0,NULL),(17,3800010000,10,0,NULL),(18,5200010000,10,0,NULL);
+/*!40000 ALTER TABLE `shopapp_order` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-19 21:39:47
